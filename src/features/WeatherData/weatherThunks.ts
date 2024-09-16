6f8672a23f189d59350ea35c11f1c512d9dda6a0
jaimehu20 <jaimehu20@gmail.com>
2024-09-16 13:31:59 +0200

    PATCH/ Fixed url

--- src/features/WeatherData/weatherThunks.ts
+++ src/features/WeatherData/weatherThunks.ts
@@ -1,19 +1,19 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getLocationData = createAsyncThunk("weatherData/getLocationData", async (cityName : string) => {
-    const request = await fetch(`http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_ACCESS_KEY}&q=${cityName}`);
+    const request = await fetch(`https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_ACCESS_KEY}&q=${cityName}`);
     const data = request.json();
     return data;
 })
 
 export const getCustomLocation = createAsyncThunk("customWeatherData/getCustomWeatherData", async (cityName : string) => {
-    const request = await fetch(`http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_ACCESS_KEY}&q=${cityName}`);
+    const request = await fetch(`https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_ACCESS_KEY}&q=${cityName}`);
     const data = request.json();
     return data;
 })
 
 export const getForecastData = createAsyncThunk("forecastData/getForecastData", async (cityName : string) => {
-    const request = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_ACCESS_KEY}&q=${cityName}&days=7`);
+    const request = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_ACCESS_KEY}&q=${cityName}&days=7`);
     const data = request.json();
     return data;
 })
